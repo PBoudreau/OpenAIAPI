ae41179931aa78f0373058e27dd7c9f855de9452
Philippe Boudreau <philippe.boudreau@theoremone.co>
2025-02-10 11:04:58 -0500

    Make OpenAIAPIConfig.init public

--- Sources/OpenAIAPI/OpenAIAPIConfig.swift
+++ Sources/OpenAIAPI/OpenAIAPIConfig.swift
@@ -14,7 +14,7 @@ public struct OpenAIAPIConfig {
     /// ideally, credentials should NOT be exposed in your code, hardcoded or not it doesn't matter
     /// set <endpoint> to your backend/reverse-proxy and don't use your real OpenAI API secret in your client app
     /// openai endpoint fallback is just for development and testing
-    init (secret: String?=nil, endpoint: String?=nil, organization: String?=nil) {
+    public init (secret: String?=nil, endpoint: String?=nil, organization: String?=nil) {
         self.secret = secret
         self.baseURL = URL(string: endpoint ?? "https://api.openai.com")!
         self.organization = organization
